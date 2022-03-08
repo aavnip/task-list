fdd5ec6d5cb11bb6d72bdc150e458ac3fdc21da6
aavnip <aavnip@chicagobooth.edu>
2022-03-08 14:43:31 +0000

    Added user data model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_08_144215) do
+ActiveRecord::Schema.define(version: 2022_03_08_144314) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
     t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
